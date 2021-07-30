f08ef2e45b577cbb52ae0d386f8232c7eb9407d9
Lais Aquino <laisaquino00@gmail.com>
2021-07-30 18:43:18 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const listaFilmes = [
+  "https://www.netflix.com/br/title/81313229",
+  "https://www.netflix.com/br/title/80180171",
+  "https://www.netflix.com/br/title/81318872",
+  "https://www.netflix.com/br/title/81159258",
+  "https://www.netflix.com/br/title/81443941",
+  "https://www.netflix.com/br/title/81452916",
+  "https://www.netflix.com/br/title/81365087",
+  "https://www.netflix.com/br/title/81397558",
+];
+
+function informacao(filme) {
+  window.open(listaFilmes[filme]);
+}
